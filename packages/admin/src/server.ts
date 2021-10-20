b0c1b0e5f64b8b296c5f11d32f7169fee6ab6545
Richard Vanbergen <rich.vanbergen@gmail.com>
2021-10-20 14:23:52 +0900

    Adds a licence to server.ts

--- packages/admin/src/server.ts
+++ packages/admin/src/server.ts
@@ -11,6 +11,7 @@ const app = express();
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
   mongoURL: process.env.MONGODB_URI,
+  license: process.env.PAYLOAD_LICENCE,
   express: app,
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
